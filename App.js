e3b4a61d79216f6aed5428ca5e2b02347d341a70
motaharmalik <58002725+motaharmalik@users.noreply.github.com>
2020-01-20 17:54:15 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+    return (
+    <View>
+      <view>
+        <view>
+          <TextInput /> 
+          <Button title="ADD"/>
+
+        </view>
+      </view>
+      
+    </View> 
+  );
+}
+
+
+const styles = StyleSheet.create({
+  
+});
+
